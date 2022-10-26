76355b2463e84ec1c5276bda71bfa08999660172
Allan Garcia <allan.garcia@gmail.com>
2022-10-26 06:06:59 -0300

    Initial Commit

---
+++ SVG Image/SVG_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SVG_ImageApp.swift
+//  SVG Image
+//
+//  Created by Allan Garcia on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SVG_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
