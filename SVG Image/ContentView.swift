57db1e3753e4e4e9562b8ed4b4c384b3f39a38ea
Allan Garcia <allan.garcia@gmail.com>
2022-10-26 06:34:55 -0300

    Github answer
    https://stackoverflow.com/questions/74201907/load-svg-string-to-uiimage-swift

--- SVG Image/ContentView.swift
+++ SVG Image/ContentView.swift
@@ -10,9 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+            Image("logo")
+                .frame(width: 100, height: 100)
             Text("Hello, world!")
         }
         .padding()
